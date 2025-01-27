4fa6db9c6ed0356eaab19cb457b5ab00fa8a9096
Wayne Chan <wayne.chan@busywhale.com>
2025-01-27 14:38:33 +0800

    add question

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,39 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
 
-function Copyright() {
-  return (
-    <Typography
-      variant="body2"
-      align="center"
-      sx={{
-        color: 'text.secondary',
-      }}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+/**
+ * 1. Render Car List with from data stored in carList and priceList,
+ *    sort the list by car price in ascending order and display them as follow:
+ *    make | model | type | price
+ *
+ * 2. Create a search box, search cars by make & model with case insensitive
+ *  e.g
+ *    - search Honda - the list renders 2 cars
+ *    - search Vezel - the list renders 1 car
+ *    - search A3 Vezel - the list renders 2 cars
+ */
 
+const carList = [
+  { id: 1, make: "Mitsubishi", model: "Lancer", type: "Used" },
+  { id: 2, make: "Honda", model: "Vezel", type: "New" },
+  { id: 3, make: "Honda", model: "Civic", type: "Used" },
+  { id: 4, make: "Audi", model: "A3", type: "New" },
+  { id: 5, make: "Audi", model: "A4", type: "Used" },
+  { id: 6, make: "Audi", model: "A7", type: "New" },
+  { id: 7, make: "BMW", model: "i8", type: "Used" },
+  { id: 8, make: "BMW", model: "i10", type: "Used" },
+];
+
+const priceList = [
+  { car_id: 8, price: 10000 },
+  { car_id: 2, price: 4000 },
+  { car_id: 3, price: 3000 },
+  { car_id: 1, price: 2000 },
+  { car_id: 4, price: 1000 },
+  { car_id: 6, price: 5000 },
+  { car_id: 5, price: 6000 },
+  { car_id: 7, price: 7000 },
+];
 export default function App() {
   return (
     <Container maxWidth="sm">
@@ -30,8 +45,7 @@ export default function App() {
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
           Material UI Create React App example in TypeScript
         </Typography>
-        <ProTip />
-        <Copyright />
+        <img src="https://raw.githubusercontent.com/waynechan-bw/charting-library-examples/master/result.gif" />
       </Box>
     </Container>
   );
